ce75130a72a021af53443a43651f42c2d273faf2
Nhewvys Batista <nhewvys.silva@gmail.com>
2023-09-22 17:41:17 -0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+const EmailUser = document.getElementById('email-user')
+const PasswordUser = document.getElementById('password-user')
+const input = document.getElementById('password-user')
+const button = document.getElementById('button')
+
+button.addEventListener('click', changeVisibilityPassword)
+input.addEventListener('input', showPassword)
+
+function changeVisibilityPassword() {
+    if (input.type == 'password') {
+        input.type = 'text'
+        input.focus()
+    } else {
+        input.type = 'password'
+        input.focus()
+    }
+}
+function showPassword() {
+    let inputv = input.value
+    if (inputv != '') {
+        button.style.visibility = 'visible' 
+    }
+    if (inputv == '') {
+        button.style.visibility = 'hidden'  
+    }
+
+}
+

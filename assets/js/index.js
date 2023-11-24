743015832203583cebc711dd8fe3c778c832dc10
Nhewvys Batista <nhewvys.silva@gmail.com>
2023-11-24 15:04:49 -0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,31 @@
-const EmailUser = document.getElementById('email-user')
-const input = document.getElementById('password-user')
-const button = document.getElementById('button')
+const EmailUser = document.getElementById('email-user');
+const input = document.getElementById('password-user');
+const button = document.getElementById('button');
+const eyeIcon = document.getElementById('eye-icon');
 
-button.addEventListener('click', changeVisibilityPassword)
-input.addEventListener('input', showPassword)
+button.addEventListener('click', changeVisibilityPassword);
+input.addEventListener('input', showPassword);
 
 function changeVisibilityPassword(evento) {
-
     evento.stopPropagation();
-    
-    if (input.type == 'password') {
-        input.type = 'text'
-        input.focus()
+
+    if (input.type === 'password') {
+        input.type = 'text';
+        eyeIcon.src = 'assets/images/eye-ish-close.svg';
+        input.focus();
     } else {
-        input.type = 'password'
-        input.focus()
+        input.type = 'password';
+        eyeIcon.src = 'assets/images/eye-ish.svg';
+        input.focus();
     }
 }
+
 function showPassword() {
-    let inputv = input.value
-    if (inputv != '') {
-        button.style.visibility = 'visible' 
+    let inputv = input.value;
+    if (inputv !== '') {
+        button.style.visibility = 'visible';
     }
-    if (inputv == '') {
-        button.style.visibility = 'hidden'  
+    if (inputv === '') {
+        button.style.visibility = 'hidden';
     }
-
 }
-
